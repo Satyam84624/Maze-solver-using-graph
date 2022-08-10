85498754162af70c0f5d3707990e02bc7d6591ee
Satyam84624 <78997574+Satyam84624@users.noreply.github.com>
2022-08-10 11:48:58 +0530

    Final Build

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.scss";
+
+const Footer = () => {
+    return ( 
+        <div className="bg-dark text-center">
+            <a href="https://www.linkedin.com/in/satyam-kumar-a154351b4/" className="fa fa-linkedin"></a>
+            <a href="https://github.com/Satyam84624" className="fa fa-github"></a>
+            {/* <a href="https://medium.com/@" className="fa fa-gfg"></a> */}
+        </div>
+     );
+}
+ 
+export default Footer;
